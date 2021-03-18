bf5a24cb01cd19dd49861f2f5358d834a8f22ee8
Kazuki Urabe <urabe0225@gmail.com>
2021-03-18 22:10:28 +0900

    modified demo files

--- scripts/demo.py
+++ scripts/demo.py
@@ -3,14 +3,9 @@
 import rospy
 import numpy as np
 from geometry_msgs.msg import PoseStamped
-from ar_func import read_csv2
-from ar_func import quaternion_to_vector
-from ar_func import PoseStamped_to_Numpyarray
-from ar_func import estimate
+from ar_func import read_csv2, quaternion_to_vector, PoseStamped_to_Numpyarray, estimate
 
-def callback(msg, args):
-    inputTvec, inputQuat, inputTQ = PoseStamped_to_Numpyarray(msg)
-    translate_matrix = args
+def common(translate_matrix, inputTQ):
     Est_Quat, Est_pos = estimate(translate_matrix, inputTQ)
     p = PoseStamped()
     p.header.frame_id = "map"
@@ -22,22 +17,18 @@ def callback(msg, args):
     p.pose.orientation.y = Est_Quat[1]
     p.pose.orientation.z = Est_Quat[2]
     p.pose.orientation.w = Est_Quat[3]
+    return p
+
+def callback(msg, args):
+    _, _, inputTQ = PoseStamped_to_Numpyarray(msg)
+    translate_matrix = args
+    p = common(translate_matrix, inputTQ)
     pub.publish(p)
 
 def callback2(msg, args):
-    inputTvec, inputQuat, inputTQ = PoseStamped_to_Numpyarray(msg)
+    _, _, inputTQ = PoseStamped_to_Numpyarray(msg)
     translate_matrix = args
-    Est_Quat, Est_pos = estimate(translate_matrix, inputTQ)
-    p = PoseStamped()
-    p.header.frame_id = "map"
-    p.header.stamp = rospy.Time.now()
-    p.pose.position.x = Est_pos[0]
-    p.pose.position.y = Est_pos[1]
-    p.pose.position.z = Est_pos[2]
-    p.pose.orientation.x = Est_Quat[0]
-    p.pose.orientation.y = Est_Quat[1]
-    p.pose.orientation.z = Est_Quat[2]
-    p.pose.orientation.w = Est_Quat[3]
+    p = common(translate_matrix, inputTQ)
     pub2.publish(p)
 
 def main():
@@ -56,10 +47,10 @@ if __name__ == '__main__':
         pose_file = pose_dir + "tmatrix_f_rb.csv"
         tmatrix = read_csv2(pose_file)
         translate_matrix = tmatrix.astype(np.float32)
-        print(translate_matrix)
+        rospy.loginfo(translate_matrix)
         reverse_matrix = np.linalg.inv(translate_matrix)
-        print(reverse_matrix)
-        print(np.dot(translate_matrix, reverse_matrix))
+        rospy.loginfo(reverse_matrix)
+        rospy.loginfo(np.dot(translate_matrix, reverse_matrix))
         pub = rospy.Publisher("/AR/demo_pose", PoseStamped, queue_size=10)
         pub2 = rospy.Publisher("/RB/demo_pose", PoseStamped, queue_size=10)
         main()
