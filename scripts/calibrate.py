37e1324fe42eec75644d8c42eeda40d254e2e82f
Kazuki Urabe <urabe0225@gmail.com>
2021-09-15 16:58:19 +0900

    classing scripts/calibrate.py

--- scripts/calibrate.py
+++ scripts/calibrate.py
@@ -5,28 +5,43 @@ import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
-def callback(msg):
-    cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
-    dst = cv2.undistort(cv_image, intrinsic, distortion)
-    image_message = bridge.cv2_to_imgmsg(dst, encoding="bgr8")
-    pub.publish(image_message)
+class Calibrator(object):
+    def __init__(self, pubtopic, subtopic, intrinsic, distortion):
+        self.publisher = rospy.Publisher(pubtopic, Image, queue_size=10)
+        self.subscriber = rospy.Subscriber(subtopic, Image, self.callback, queue_size=10)
+        self.message = Image()
+        self.bridge = CvBridge()
+        self.intrinsic = intrinsic
+        self.distortion = distortion
+
+    def callback(self,msg):
+        cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        dst = cv2.undistort(cv_image, self.intrinsic, self.distortion)
+        self.message = self.bridge.cv2_to_imgmsg(dst, encoding="bgr8")
+        self.publish()
+
+    def publish(self):
+        self.publisher.publish(self.message)
 
 def main():
     try:
-        rospy.init_node(NODE_NAME)
-        rospy.Subscriber("image_raw", Image, callback, queue_size=10)
-        rospy.spin()
+        node = Calibrator(pubtopic, subtopic, intrinsic, distortion)
+        while not rospy.is_shutdown():
+            rospy.sleep(0.1)
     except rospy.ROSInterruptException:
         pass
 
 if __name__ == '__main__':
     try:
         NODE_NAME = 'calibration'
-        bridge = CvBridge()
-        pub = rospy.Publisher("calib_image", Image, queue_size=10)
+        pubtopic = "calib_image"
         fs = cv2.FileStorage(rospy.get_param("calibration_path"), cv2.FILE_STORAGE_READ)
         intrinsic = fs.getNode("intrinsic").mat()
         distortion = fs.getNode("distortion").mat()
+        name = rospy.get_param("name", "camera")
+        subtopic = "/" + name + "/image_raw"
+
+        rospy.init_node(NODE_NAME)
         main()
     except KeyboardInterrupt:
         pass
